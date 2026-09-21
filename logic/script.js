7cece5beb522a96e350710aac72b305574d72486
Aayushgurav20 <adwarslucifer@gmail.com>
2026-09-21 20:33:05 +0530

    trying new logic

--- logic/script.js
+++ logic/script.js
@@ -1,21 +1,4 @@
-// function reverseAstring(input){
-
-//   // if(typeof input != "string"){
-//   //   throw new Error("the input should be a string")
-//   // }
-//   let end = "";
-//   for(let i = input.length-1; i >= 0; i--){
-//     end += input[i]
-//   }
-//   return end;
-// }
-
-// console.log(reverseAstring("Madhusmita"))
-// console.log(reverseAstring(1286286))
-
-// This is a quite NON-CONFUSING way to writign the same code yet with a simpler approach but teh upper one was very much used for case sensetive for E.G number or any other value but this code is not that case sensetive //
-
-function reverseAstring2(input){
+function reverseAstring2(input) {
   const arr = input.split('');
   const reversedArray = arr.reverse();
   const reversed = reversedArray.join('')
